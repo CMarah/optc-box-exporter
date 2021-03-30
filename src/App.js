4212204c4c48fa204004ec6b91502543cc32ae5f
Carlos <carlos@marah.dev>
2021-03-30 20:40:57 +0200

    Switch to React frontend

---
+++ src/App.js
@@ -0,0 +1,127 @@
+import './App.css';
+
+const processInput = require('./plain.js');
+const UNITS_DATA = require('./data.js');
+
+const purl = process.env.PUBLIC_URL;
+const END_K = UNITS_DATA.length;
+
+const loadInput = e => {
+  const imgElement = document.getElementById('imageOriginal');
+  imgElement.src = URL.createObjectURL(e.target.files[0]);
+};
+
+const App = () => {
+  return (<>
+    <div className="container">
+      <div className="jumbotron">
+        <h1>OPTC Box Importer</h1>
+        <p>This website uses OpenCV.js to detect your OPTC box contents.</p>
+        <img id="scorner" src={purl + "/images/scorner.png"} style={{display: 'none'}} alt=""/>
+        <img id="dcorner" src={purl + "/images/dcorner.png"} style={{display: 'none'}} alt=""/>
+        <img id="qcorner" src={purl + "/images/qcorner.png"} style={{display: 'none'}} alt=""/>
+        <img id="pcorner" src={purl + "/images/pcorner.png"} style={{display: 'none'}} alt=""/>
+        <img id="icorner" src={purl + "/images/icorner.png"} style={{display: 'none'}} alt=""/>
+        <img id="xcorner" src={purl + "/images/xcorner.png"} style={{display: 'none'}} alt=""/>
+        <ul id="compares">
+          <img id="compare" alt=""/>
+          {(new Array(END_K)).fill(0).map((x,k) => (
+            <img id={`compare${k+1}`} key={k} style={{display: "none"}} alt=""
+              src={purl + `/portraits/${k+1}.png`}
+            />
+          ))}
+        </ul>
+      </div>
+      <div className="row">
+        <div className="col-sm">
+          <div className="card">
+            <div className="card-header">
+              Original Image
+            </div>
+            <div className="card-block text-center">
+              <img id="imageOriginal" alt="Upload"/>
+            </div>
+            <div className="card-footer text-muted">
+              <input type="file" id="imageInput" name="file" onChange={loadInput}/>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm">
+          <div className="card">
+            <div className="card-header">
+              Modified Image
+            </div>
+            <div className="card-block">
+              <canvas id="imageCanvas1"></canvas>
+              <canvas id="imageCanvas1b"></canvas><br/>
+              <canvas id="imageCanvas2"></canvas>
+              <canvas id="imageCanvas2b"></canvas><br/>
+              <canvas id="imageCanvas3"></canvas>
+              <canvas id="imageCanvas3b"></canvas><br/>
+              <canvas id="imageCanvas4"></canvas>
+              <canvas id="imageCanvas4b"></canvas><br/>
+              <canvas id="imageCanvas5"></canvas>
+              <canvas id="imageCanvas5b"></canvas><br/>
+              <canvas id="imageCanvas6"></canvas>
+              <canvas id="imageCanvas6b"></canvas><br/>
+              <canvas id="imageCanvas7"></canvas>
+              <canvas id="imageCanvas7b"></canvas><br/>
+              <canvas id="imageCanvas8"></canvas>
+              <canvas id="imageCanvas8b"></canvas><br/>
+              <canvas id="imageCanvas9"></canvas>
+              <canvas id="imageCanvas9b"></canvas><br/>
+              <canvas id="imageCanvas10"></canvas>
+              <canvas id="imageCanvas10b"></canvas><br/>
+              <canvas id="imageCanvas11"></canvas>
+              <canvas id="imageCanvas11b"></canvas><br/>
+              <canvas id="imageCanvas12"></canvas>
+              <canvas id="imageCanvas12b"></canvas><br/>
+              <canvas id="imageCanvas13"></canvas>
+              <canvas id="imageCanvas13b"></canvas><br/>
+              <canvas id="imageCanvas14"></canvas>
+              <canvas id="imageCanvas14b"></canvas><br/>
+              <canvas id="imageCanvas15"></canvas>
+              <canvas id="imageCanvas15b"></canvas><br/>
+              <canvas id="imageCanvas16"></canvas>
+              <canvas id="imageCanvas16b"></canvas><br/>
+              <canvas id="imageCanvas17"></canvas>
+              <canvas id="imageCanvas17b"></canvas><br/>
+              <canvas id="imageCanvas18"></canvas>
+              <canvas id="imageCanvas18b"></canvas><br/>
+              <canvas id="imageCanvas19"></canvas>
+              <canvas id="imageCanvas19b"></canvas><br/>
+              <canvas id="imageCanvas20"></canvas>
+              <canvas id="imageCanvas20b"></canvas>
+            </div>
+            <button type="button" id="processBtn" className="btn btn-primary"
+              onClick={processInput}
+            >Detect</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="modal"></div>
+    <script src="data.js" type="text/javascript"></script>
+    {/*<script src="index.js" type="text/javascript"></script>
+    <script async src="opencv.js" onLoad={onOpenCvReady} type="text/javascript"></script>
+    <script src="plain.js" type="text/javascript"></script>
+    {/*<div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>*/}
+  </>);
+}
+
+export default App;
---
+++ src/download.js
@@ -0,0 +1,121 @@
+const fs    = require('fs')
+const fetch = require('isomorphic-fetch');
+
+const download = async (id, url) => {
+  const path = `./portraits/${id}.png`;
+  const res = await fetch(url);
+  const fileStream = fs.createWriteStream(path);
+  //TODO check for 404
+  return new Promise((resolve, reject) => {
+    res.body.pipe(fileStream);
+    res.body.on("error", reject);
+    fileStream.on("finish", resolve);
+  });
+};
+
+const basicUrl = k => `https://onepiece-treasurecruise.com/wp-content/uploads/${k}`;
+const getThumbnailUrl = n => {
+  switch (n) {
+    case 9001: return basicUrl('gskull_luffy.png'); break;
+    case 9002: return basicUrl('gskull_zoro.png'); break;
+    case 9003: return basicUrl('gskull_nami.png'); break;
+    case 9004: return basicUrl('gskull_usopp_f.png'); break;
+    case 9005: return basicUrl('gskull_sanji_f.png'); break;
+    case 9006: return basicUrl('gskull_chopper_f.png'); break;
+    case 9007: return basicUrl('gskull_robin_f.png'); break;
+    case 9008: return basicUrl('gskull_franky_f.png'); break;
+    case 9009: return basicUrl('gskull_brook_f.png'); break;
+    case 9010: return basicUrl('gred_skull_f.png'); break;
+    case 9011: return basicUrl('gblue_skull_f.png'); break;
+    case 9012: return basicUrl('gyellow_skull2_f.png'); break;
+    case 9013: return basicUrl('ggreen_skull2_f.png'); break;
+    case 9014: return basicUrl('gblack_skull_f.png'); break;
+    case 9015: return basicUrl('gJerma_skull_f1.png'); break;
+    case 9016: return basicUrl('gJerma_skull_f2.png'); break;
+    case 9017: return basicUrl('gJerma_skull_f3.png'); break;
+    case 9018: return basicUrl('gJerma_skull_f4.png'); break;
+    case 9019: return basicUrl('gJerma_skull_f5.png'); break;
+    case 9020: return basicUrl('gDoflamingo_skull_f.png'); break;
+    case 9021: return basicUrl('genel_skull_f.png'); break;
+    case 9022: return basicUrl('ghiguma_skull_f.png'); break;
+    case 9023: return basicUrl('gsanji_skull_f.png'); break;
+    case 9024: return basicUrl('gfrankie_skull_f.png'); break;
+    case 9025: return basicUrl('gCavendish_skull_f.png'); break;
+    case 9026: return basicUrl('gDoflamingo_skull_f2.png'); break;
+    case 9027: return basicUrl('gJerma_skull_f6.png'); break;
+    case 9028: return basicUrl('gJerma_skull_f7.png'); break;
+    case 9029: return basicUrl('gJerma_skull_f8.png'); break;
+    case 9030: return basicUrl('gJerma_skull_f9.png'); break;
+    case 9031: return basicUrl('gHancock_skull_f.png'); break;
+    case 9032: return basicUrl('gnami_skull_f.png'); break;
+  }
+  if (n === null || n === undefined) return '';
+  const id = ('0000' + n).slice(-4).replace(/(057[54])/, '0$1'); // missing aokiji image
+  switch (id) {
+    case '0742': return basicUrl('f0742-2.png'); break;
+    case '3000': return basicUrl('f3000_1.png'); break;
+    case '3333': return 'http://onepiece-treasurecruise.com/en/wp-content/uploads/sites/2/f5013.png'; break;
+    case '3334': return 'http://onepiece-treasurecruise.com/en/wp-content/uploads/sites/2/f5014.png'; break;
+    case '3339': return 'res/character_10852_t1.png'; break;
+    case '3340': return 'res/character_10853_t1.png'; break;
+    case '3347': return 'res/character_1508_t1.png'; break;
+    case '3348': return 'res/character_1509_t1.png'; break;
+    case '3349': return 'res/character_1510_t1.png'; break;
+    case '3350': return 'res/character_1511_t1.png'; break;
+    case '3351': return 'res/character_10861_t1.png'; break;
+    case '3352': return 'res/character_10862_t1.png'; break;
+    case '3353': return 'res/character_10994_t1.png'; break;
+    case '3354': return 'res/character_10995_t1.png'; break;
+    case '3356': return 'res/character_10869_t1.png'; break;
+    case '3357': return 'res/character_10870_t1.png'; break;
+    case '3358': return 'res/character_10867_t1.png'; break;
+    case '3359': return 'res/character_10868_t1.png'; break;
+    case '3360': return 'res/character_11037_t1.png'; break;
+    case '3361': return 'res/character_11038_t1.png'; break;
+    case '2768': return 'res/character_10258_t1.png'; break;
+    case '2769': return 'res/character_10259_t1.png'; break;
+    case '2770': return 'res/character_10262_t1.png'; break;
+    case '2771': return 'res/character_10263_t1.png'; break;
+    case '3366': return 'res/character_10858_t1.png'; break;
+    case '3367': return 'res/character_10859_t1.png'; break;
+    case '3368': return 'res/character_10860_t1.png'; break;
+    case '3370': return 'http://onepiece-treasurecruise.com/en/wp-content/uploads/sites/2/f5052.png'; break;
+    case '3371': return 'res/character_11243_t.png'; break;
+    case '3372': return 'res/character_11244_t.png'; break;
+    case '3373': return 'res/character_11245_t.png'; break;
+    case '3374': return 'http://onepiece-treasurecruise.com/en/wp-content/uploads/sites/2/f5053.png'; break;
+    case '3375': return 'res/character_10863_t.png'; break;
+    case '3376': return 'res/character_10864_t.png'; break;
+    case '3380': return 'res/character_11333_t1.png'; break;
+    case '3381': return 'res/KDugejE.png'; break;
+    case '3382': return 'res/character_11615_t1.png'; break;
+    case '3383': return 'res/character_11760_t.png'; break;
+    case '3384': return 'res/character_11400_t1.png'; break;
+    case '3385': return 'res/character_11338_t1.png'; break;
+    default: break;
+  }
+  return 'https://onepiece-treasurecruise.com/wp-content/uploads/f' + id + '.png';
+};
+
+const downloadImg = n => {
+  const url = getThumbnailUrl(n);
+  if (!url.includes("res/")) {
+    return download(n, url);
+  } else {
+    return fs.copyFileSync(url, `portraits/${n}.png`);
+  }
+};
+
+const downloadAllPortraits = async () => {
+  const NUMBER_OF_PORTRAITS = 3400;
+  for (let i = 1; i <= NUMBER_OF_PORTRAITS; ++i) {
+    if (!fs.existsSync(`./portraits/${i}.png`)) {
+      console.log(`Downloading ${i}`);
+      await downloadImg(i);
+      console.log(`✅ Done ${i}!`);
+    } else {
+      console.log(`✅ Already had ${i}!`);
+    }
+  }
+};
+downloadAllPortraits();
